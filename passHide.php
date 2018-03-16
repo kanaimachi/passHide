36f6d745426575dd7618964bf7bfddae65aeac54
kanaimachi <kanaimachi@gmail.com>
2018-03-16 16:29:26 +0900

    A test program to confirm .gitignore. files under in "hidden" directory will be ignored other files and directorys will be

---
+++ passHide.php
@@ -0,0 +1,14 @@
+﻿<?php
+	$passFile="./hidden/pass.json";
+	$json = file_get_contents($passFile);
+	$passObj=json_decode($json);
+	echo $passObj->basic[0]->id.":".$passObj->basic[0]->pass."<br>";
+	var_dump($passObj);
+
+	echo "<br>";
+
+	$dataFile="./data/data.json";
+	$json = file_get_contents($dataFile);
+	$dataObj=json_decode($json);
+	var_dump($dataObj);
+?>
